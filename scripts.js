cf45fde450f17edf83c4e052683970f7b4910880
lou-pastorino <168196916+lou-pastorino@users.noreply.github.com>
2024-05-07 14:04:06 +0100

    Finished Website v1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Burger icon being clicked
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
